3c844001f2fdd9f670c282872e0750ad25e56d44
Ahryun Moon <ansdkfus13@gmail.com>
2014-04-22 18:54:34 -0700

    Email added to home page

--- js/pieces.js
+++ js/pieces.js
@@ -7,4 +7,23 @@ $(document).ready(function() {
 	$('.pc-small .pc-play-button').click(function() {
 		$("#pc-video-modal-sm").modal('show');
 	});
+
+	
+	var z1n0mr910ge1jsh;(function(d, t) {
+	var s = d.createElement(t), options = {
+	'userName':'getpieces',
+	'formHash':'z1n0mr910ge1jsh',
+	'autoResize':true,
+	'height':'517',
+	'async':true,
+	'host':'wufoo.com',
+	'header':'show',
+	'ssl':true};
+	s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+	s.onload = s.onreadystatechange = function() {
+	var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+	try { z1n0mr910ge1jsh = new WufooForm();z1n0mr910ge1jsh.initialize(options);z1n0mr910ge1jsh.display(); } catch (e) {}};
+	var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+	})(document, 'script');
+
 });
